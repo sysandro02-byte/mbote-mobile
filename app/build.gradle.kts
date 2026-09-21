d862ce2162b79f02b965935b8b14bfcfe668da1f
loukatech <sysandro02@gmail.com>
2026-09-21 03:21:33 +0100

    build(realtime): remove obsolete Socket.IO Android dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -192,9 +192,6 @@ dependencies {
     implementation(libs.room.ktx)
     implementation(libs.okhttp)
     implementation(libs.webrtc)
-    implementation("io.socket:socket.io-client:2.1.1") {
-        exclude(group = "org.json", module = "json")
-    }
     implementation(libs.androidx.biometric)
     ksp(libs.room.compiler)
 
